e9e439417c6e717689144a80ebead06f30e30880
Vincent Durham <vdurham2244@gmail.com>
2025-03-11 23:38:56 -0400

    A fresh start

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/drone-game/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    },
     server: {
         open: true
-    },
-    base: '/drone-game/'
+    }
 }); 
