d90242993d555bead6d04938121bd00349a12ddc
Vincent Durham <vdurham2244@gmail.com>
2025-03-12 00:12:58 -0400

    hope

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,16 @@ export default defineConfig({
         outDir: 'dist',
         assetsDir: '',
         rollupOptions: {
+            input: 'index.html',
             output: {
                 entryFileNames: 'index.js',
                 chunkFileNames: '[name]-[hash].js',
                 assetFileNames: '[name].[ext]'
             }
-        }
+        },
+        sourcemap: false,
+        minify: 'terser',
+        target: 'es2015'
     },
     server: {
         open: true
