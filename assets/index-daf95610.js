7f50dc90de09c5f3d04cc347fd68cafcefed9a37
Vincent Durham <vdurham2244@gmail.com>
2025-03-11 22:07:06 -0400

    Updates

--- assets/index-daf95610.js
+++
@@ -1,2 +0,0 @@
-export function __vite_legacy_guard(){import.meta.url,import("_").catch(()=>1);async function*t(){}}(function(){"use strict"})();
-//# sourceMappingURL=index-daf95610.js.map
--- assets/index-legacy-518b8698.js
+++
@@ -1,2 +0,0 @@
-System.register([],(function(e,t){"use strict";return{execute:function(){}}}));
-//# sourceMappingURL=index-legacy-518b8698.js.map
